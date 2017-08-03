b6e9a10aa428a1eab2673863217be24ace0e243d
Vivek1110 <30702287+Vivek1110@users.noreply.github.com>
2017-08-04 00:35:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
