6fb52a31aafa49a379924b206bb0d2a21a90fbe9
vivekjeevakumar <vivekjeevakumar1110@outlook.com>
2017-08-26 16:14:10 +0530

    Completed the module  2

--- server.js
+++ server.js
@@ -5,54 +5,87 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title: 'Article One |  Vivek',
-    heading: 'Article One',
-    date: 'Todays Date',
-    content: `  <p>
-                    This is the content paragraph. You can type anything here.
-                </p>
-                <p>
-                    This is the content paragraph. You can type anything here.
-                </p>
-                <p>
-                    This is the content paragraph. You can type anything here.
-                </p>
-                <p>
-                    This is the content paragraph. You can type anything here.
-                </p>
-                <p>
-                    This is the content paragraph. You can type anything here.
-                </p>`
-};
+var articles =  {
+    'article-one' : {
+        title: 'Article One |  Vivek',
+        heading: 'Article One',
+        date: 'Todays Date as now',
+        content: `  <p>
+                        This is the content paragraph. You can type anything here.
+                    </p>
+                    <p>
+                        This is the content paragraph. You can type anything here.
+                    </p>
+                    <p>
+                        This is the content paragraph. You can type anything here.
+                    </p>
+                    <p>
+                        This is the content paragraph. You can type anything here.
+                    </p>
+                    <p>
+                        This is the content paragraph. You can type anything here.
+                    </p>`
+    },
+    'article-two' : {
+        title: 'Article Two |  Vivek',
+        heading: 'Article two',
+        date: 'Todays Date as now',
+        content: `  <p>
+                        This is the content paragraph. You can type anything here.
+                    </p>
+                    <p>
+                        This is the content paragraph. You can type anything here.
+                    </p>`
 
-var htmlTemplate = `<html>
-    <head>
-        <title>
-            Article one | Vivek
-        </title>
-        <meta name="viewport" content="width=device-width, intial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-      <div class="container">
-              <div>
-                <a href="/">Home</a>
+    },
+    'article-three' : {
+        title: 'Article Three |  Vivek',
+        heading: 'Article Three',
+        date: 'Todays Date as now',
+        content: `  <p>
+                        This is the content paragraph. You can type anything here.
+                    </p>
+                    <p>
+                        This is the content paragraph. You can type anything here.
+                    </p>`
+
+    }
+
+}
+
+function createTemplate(data) {
+    title= data.title;
+    heading =  data.heading;
+    date = data.date;
+    content = data.content;
+    var htmlTemplate = `<html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, intial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+          <div class="container">
+                  <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                  ${content}
+                </div>
             </div>
-            <hr/>
-            <h3>
-                Article One
-            </h3>
-            <div>
-                Todays Date.
-            </div>
-            <div>
-              ${content}
-            </div>
-        </div>
-    </body>
-</html>
-`
+        </body>
+    </html>`;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -69,19 +102,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName', function (req, res) {
+   var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
-
-app.get('/article-two', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-
-app.get('/article-three', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
